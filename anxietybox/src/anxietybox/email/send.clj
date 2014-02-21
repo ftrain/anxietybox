680c8d8ecef5476842bfcacc87df69ebb5b03464
Paul Ford <ford@ftrain.com>
2014-02-21 16:18:43 -0500

    Created activation emails.

--- anxietybox/src/anxietybox/email/send.clj
+++ anxietybox/src/anxietybox/email/send.clj
@@ -1,26 +1,35 @@
 (ns anxietybox.email.send
-  (:use compojure.core)
   (:require
     [anxietybox.db :as db]
     [clojurewerkz.mailer.core :as mail]))
 
 ;; set default delivery mode (:smtp, :sendmail or :test)
 (mail/delivery-mode! :sendmail)
+(def default-email "Your Anxiety <your.anxiety@anxietybox.com>")
 
 (defn send-confirmation [box]
+  (mail/deliver-email {:from default-email
+                        :to [(:email box)]
+                        :subject (:subject "[AnxietyBox.com] Confirmation requested")}
+    "templates/activate.mustache"
+    box
+  :text/html))
 
-  )
+(defn send-reminder [box]
+  (mail/deliver-email {:from default-email
+                        :to [(:email box)]
+                        :subject (:subject "[AnxietyBox.com] Account info")}
+    (if (:active box)"templates/deactivate.mustache" "templates/activate.mustache")
+    box
+  :text/html))
 
     
 (defn send-anxiety [box anxiety]
-  (mail/deliver-email {:from "Your Anxiety <your.anxiety@anxietybox.com>"
+  (mail/deliver-email {:from default-email
                         :to [(:email box)]
                         :subject (:subject anxiety)}
     "templates/anxiety.mustache"
     (merge box anxiety)
   :text/html))
 
-(send-email
-  (db/box-select "ford@ftrain.com")
-  {:subject "You are a terrible failure."})
 
