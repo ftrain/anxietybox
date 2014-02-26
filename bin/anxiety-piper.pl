c0d32fcfa3a62a65c7f44e25527a9b034a350498
Paul Ford <ford@ftrain.com>
2014-02-26 19:59:45 +0000

    Tool that can accept email pipes.

---
+++ bin/anxiety-piper.pl
@@ -0,0 +1,9 @@
+#!/bin/env perl
+$email = "";
+while (<STDIN>) { 
+    die "TOO MANY LINES" if 1000 > $x++; 
+    $email .= $_;
+}
+$email =~ s/'/\\'/;
+
+open (F, "|curl -F'auth=100&email=$email' --url http://anxietybox.com/reply");
