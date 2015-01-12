cee2cad8c654b47578c65a292f8eee416c483699
Paul Ford <ford@ftrain.com>
2015-01-11 23:09:18 -0500

    Updated project dependencies to latest versions.

--- anxietybox/project.clj
+++ anxietybox/project.clj
@@ -2,22 +2,23 @@
   :description "Code for Anxietybox.com"
   :url "http://anxietybox.com/source"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                  [compojure "1.1.6"]
-                  [org.clojure/java.jdbc "0.3.3"]
+                  [compojure "1.3.1"]
+                  [org.clojure/java.jdbc "0.3.6"]
                   [enlive "1.1.5"]
                   [postgresql/postgresql "8.4-702.jdbc4"]
                   [hiccup "1.0.5"]
-                  [garden "1.1.5"]
-                  [clj-http "0.9.0"]
-                  [cheshire "5.3.1"]
-                  [com.taoensso/timbre "3.1.1"]
-                  [lein-environ "0.4.0"]
-                  [environ "0.4.0"]
+                  [garden "1.2.5"]
+                  [clj-http "1.0.1"]
+                  [cheshire "5.4.0"]
+                  [com.taoensso/timbre "3.3.1"]
+                  [lein-environ "1.0.0"]
+                  [environ "1.0.0"]
                                         ; project.clj
                   ]
-  :plugins [[lein-ring "0.8.10"]]
+  :plugins [[lein-ring "0.9.0"]]
   :ring {:handler anxietybox.handler/app}
   :resource-paths ["src/resources"]
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+  )
+
+;; :profiles {:dev [[javax.servlet/servlet-api "2.5"]
+;;                     [ring-mock "0.1.5"]]}
