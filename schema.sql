cee9286931465f345e22aec162fb8610a00b6977
Paul Ford <ford@ftrain.com>
2015-01-14 23:06:04 -0500

    Moved to plurals in table names.

--- schema.sql
+++ schema.sql
@@ -2,10 +2,14 @@ DROP TABLE IF EXISTS account CASCADE;
 DROP TABLE IF EXISTS anxiety CASCADE;
 DROP TABLE IF EXISTS reply CASCADE;
 
+DROP TABLE IF EXISTS accounts CASCADE;
+DROP TABLE IF EXISTS anxieties CASCADE;
+DROP TABLE IF EXISTS replies CASCADE;
+
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- The account of a user
-CREATE TABLE account (
+CREATE TABLE accounts (
        created_time TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(),
        id SERIAL PRIMARY KEY,
        name VARCHAR(100) NOT NULL,
@@ -15,24 +19,24 @@ CREATE TABLE account (
        active BOOLEAN DEFAULT FALSE
 );
 
-CREATE UNIQUE INDEX lower_email_index ON account (lower(email));
-CREATE UNIQUE INDEX confirm_index ON account (confirm);
+CREATE UNIQUE INDEX lower_email_index ON accounts (lower(email));
+CREATE UNIQUE INDEX confirm_index ON accounts (confirm);
 
 -- Track anxieties
-CREATE TABLE anxiety (
+CREATE TABLE anxieties (
        id SERIAL PRIMARY KEY,
-       account_id int REFERENCES account(id) ON DELETE CASCADE,
+       account_id int REFERENCES accounts(id) ON DELETE CASCADE,
        tracker uuid DEFAULT UUID_GENERATE_V4(),
        description text UNIQUE NOT NULL
 );
 
-CREATE UNIQUE INDEX track_index ON account (confirm);
+CREATE UNIQUE INDEX track_index ON accounts (confirm);
 
 -- Track replies
-CREATE TABLE reply (
+CREATE TABLE replies (
        created_time TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(),
        id SERIAL PRIMARY KEY,
-       account_id int REFERENCES account(id),
-       anxiety_id int REFERENCES anxiety(id) ON DELETE CASCADE,
+       account_id int REFERENCES accounts(id),
+       anxiety_id int REFERENCES anxieties(id) ON DELETE CASCADE,
        description text
 );
