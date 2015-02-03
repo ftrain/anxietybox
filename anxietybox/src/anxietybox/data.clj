ee29ff727ce49140104d94e3f14c9ab0e70cc862
Paul Ford <ford@ftrain.com>
2015-02-02 23:12:25 -0500

    Updated set of utility functions for munging data.

--- anxietybox/src/anxietybox/data.clj
+++ anxietybox/src/anxietybox/data.clj
@@ -6,140 +6,184 @@
     [taoensso.timbre :as timbre]    
     [clojure.java.jdbc :as sql]))
 
+(import '(org.postgresql.util PGobject))
 ;; Logging prefix
 (timbre/refer-timbre)
 (timbre/set-config! [:appenders :spit :enabled?] true)
 (timbre/set-config! [:shared-appender-config :spit-filename] (env/env :log-file))
-;;
+(timbre/set-level! :debug)
+
+(defn str->pgobject
+  [type value]
+  (doto (PGobject.)
+    (.setType type)
+    (.setValue value)))
 
 ;; Database config
-(korma-db/defdb db (korma-db/postgres {:db (env/env :postgres-database)
-                                     :user (env/env :postgres-user)
-                                     :password (env/env :postgres-password)}))
+(korma-db/defdb db
+  (korma-db/postgres {:db (env/env :postgres-database)
+                       :user (env/env :postgres-user)
+                       :password (env/env :postgres-password)}))
 
-(korma/defentity accounts
-  (korma/entity-fields :created_time :id :name :email :count :confirm :active))
-(korma/defentity anxieties)
-(korma/defentity replies)
+;; The database is three tables: account, anxiety, and reply.
+;; We don't use passwords or anything else. This is controlled
+;; purely via email.
+
+(declare account anxiety reply mail_to_send)
+(korma/defentity account
+  (korma/entity-fields [:id :account_id] :name :email [:tracker :account_tracker] :active)
+  (korma/has-many anxiety)  
+  (korma/prepare
+    (fn [{tracker :tracker :as v}]
+      (assoc v :tracker (str->pgobject "uuid" tracker)))))
+
+;; Tracker is a UUID that we can use to track
+;; emails. People never give you an honest email and they reply
+;; using multiple emails. So when we send an anxiety we'll
+;; include a unique ID that we can put into the email prefixed by
+;; anxious: or some similar identifier; we can parse that out via
+;; regex and use it to look up the anxiety--and then we'll know the
+;; account, and we'll be able to store the reply.
+
+(korma/defentity anxiety
+  (korma/entity-fields :created_time [:id :anxiety_id] :account_id :tracker :description)
+  (korma/belongs-to account)
+  (korma/has-many reply))
+
+(korma/defentity reply
+  (korma/entity-fields :created_time [:id :reply_id] :account_id :anxiety_id :body)
+  (korma/belongs-to anxiety))
+
+(korma/defentity mail_to_send
+  (korma/entity-fields [:id :mail_id] :anxiety_id :send_time :sent)
+  (korma/belongs-to anxiety))
 
 (defn account-insert
   "Create a user account."
-  [{name :name email :email :as account}]
-  (do
-    (log "[account]" account)
-    {:result 
+  [the-account]
+  (if-let [db-account (first
+                        (korma/select account
+                          (korma/where (select-keys the-account [:email]))))]
+    (merge db-account {:existed true})
+    (do
+      (log :info "[data/account-insert]" the-account)
       (try
-        (korma/insert accounts (korma/values account))
+        (korma/insert account (korma/values the-account))
         (catch Exception e
-          (do
-            (let [e-str (str e)]
-              (log "[account-insert error]" e-str
-                {:error e-str}))))}))
+          (log :debug "[data/account-insert error]" (str e) the-account))))))
 
-; (account-insert {:name "paul ford" :email "ford@ftrain.com"})
+(defn account-activate
+  "Given a UUID set the account to active."
+  [tracker]
+  (korma/update account
+    (korma/set-fields {:active true})    
+    (korma/where {:tracker (str->pgobject "uuid" tracker)})))
 
-(defn account-select-by-confirm
+(defn anxiety-insert
+  "Make an anxiety (as a string) and associate it with an account."
+  [the-account the-anxiety]
+  (let [data {:account_id (:id the-account) :description the-anxiety}]
+    (if-let [db-anxiety (first (korma/select anxiety (korma/where data)))]
+      (merge db-anxiety {:existed true})
+      (try
+        (korma/insert anxiety (korma/values data))
+        (catch Exception e
+          (log :debug "[data/anxiety-insert error]" (str e) data))))))
+
+(defn insert-from-csv!
   ""
-  [confirm]
-  (do
-    (log "[account-select-by-confirm]" confirm)
-    (account-relate
-      (first
-        (sql/query pg
-          ["select * from account where confirm=?" confirm])))))
+  [timestamp name email anxiety_one anxiety_two description]
+  (let [the-account (account-insert {:name name :email email})]
+    (map (partial anxiety-insert the-account) [anxiety_one anxiety_two])))
 
-(defn account-select-by-email
-  "Fetch a full record for a account.
-
-   (account-select \"ford@ftrain.com\")
-
-   => {:anxieties [{:description \"finishing my book\", :account_id
-   2, :id 1} {:description \"losing weight\", :account_id 2, :id 2}
-   {:description \"making friends\", :account_id 2, :id 3}], :active
-   true, :confirm #uuid
-   \"26ed5e80-ff88-424d-a57b-7e4359ad56bf\", :count 0, :email
-   \"ford@ftrain.com\", :name \"Paul\", :id 2, :created_time #inst
-   \"2014-03-01T11:55:39.631064000-00:00\"}"
-  [email]
-  (do
-    (log "[account-select-by-email]" email)
-    (account-relate (first (sql/query pg ["select * from account where lower(email) = lower(?)" email])))))
-
-
-  
-  (defn anxiety-insert
-  "Add an anxiety to the account."
-  [{account-id :id :as the-account} {text} anxiety]
-  (sql/insert! pg "anxiety"
-    {:description (second anxiety) :account_id (:id account)}))
-
-
-(defn reply-insert
+(defn anxiety-insert-many
   ""
-  [reply]
-  (sql/insert! pg "reply" reply))
+  [the-account the-anxieties]
+  (map (partial anxiety-insert the-account) the-anxieties))
 
-(defn reply-select
+(defn anxiety-delete
   ""
-  [confirm]
-  (let [account-id (:id (first (sql/query pg ["select id from account where confirm = ?" confirm])))]
-    (sql/query pg ["select * from reply where account_id = ? ORDER BY created_time DESC" account-id])))
-
-(defn reply-select-by-account [account]
-  (vec (sql/query pg ["select * from reply where account_id = ? ORDER BY created_time DESC" (:id account)])))
-
-(defn anxiety-select [account]
-  (vec (sql/query pg
-    ["select * from anxiety where account_id = ?" (:id account)])))
-
-(defn account-relate
-  ""
-  [account]
-  (merge account 
-    {:replies (reply-select-by-account account)}
-    {:anxieties (anxiety-select account)}))
-
-
-(defn account-update
-  ""
-  [account]
-  (sql/update! pg "account" (dissoc account :id) ["id=?" (:id account)]))
+  [the-account the-anxiety]
+  (korma/delete anxiety (korma/where (select-keys the-anxiety [:id]))))
 
 (defn anxiety-update
   ""
-  [anxiety account]
-  (sql/insert! pg "anxiety"
-    (assoc (dissoc anxiety :id) {:account_id (:id account)})))
+  [the-account the-anxiety new-anxiety]
+  (korma/update anxiety
+    (korma/where (select-keys the-anxiety [:id]))
+    (korma/values {:description new-anxiety})))
 
-(defn toggle-block [code bool]
+(defn get-account-by
   ""
-  (sql/update! pg "account"
-    {:active bool}
-    ["confirm=?" code]))
+  [where-map]
+  (first
+    (korma/select account
+      (korma/with anxiety)
+      (korma/where where-map))))
 
-(defn account-activate
-  ""
-  [code]
-  (toggle-block code true))
+;Every 8 hours we want to send a user 1 emails.
+;get all the users 
+;if we haven't sent an email by then we should.
 
-(defn account-deactivate
-  ""
-  [code]
-  (toggle-block code false))
 
-(defn account-delete
-  ""
-  [code]
-  (sql/delete! pg "account" ["confirm=?" code]))
+;select from account, count (log)
+;where needs_anxiety > 0;
 
-(defn anxiety-enhance
-  ""
-  [account]
-  (merge account {:anxieties (anxiety-select account)}))
-
-(defn accounts-for-update
+(defn make-fake-account
   ""
   []
-  (map anxiety-enhance (sql/query pg ["SELECT * from account where active=?" true])))
+  (let [no (int (* 10000000 (rand)))]
+    (hash-map
+      :name (str "Paul " no)
+      :email (str "ford+" no "@ftrain.com"))))
+
+(def fake-anxieties ["My colon will explode."
+               "I won't be able to lose weight."
+               "I'll die alone."
+               "I'm a bad father."])
+
+(defn make-many-fake-accounts
+  []
+  (map (fn [fake-acct]
+         (let [the-account (account-insert fake-acct)]
+           (anxiety-insert-many the-account fake-anxieties)))
+    (take 100 (repeatedly make-fake-account))))
+
+(defn fetch-emails-to-send
+  []
+  (korma/select mail_to_send
+    (korma/with anxiety
+      (korma/with account))
+    (korma/where {:sent false
+                   :send_time [<= (korma/sqlfn now)]} )))
+
+(defn fetch-accounts-to-intro
+  []
+  (korma/select account
+    (korma/where {:active false})))
+
+(defn send-opt-in-email
+  [acct]
+  (prn
+    (str "Sending opt-in email to " (:email acct))))
+
+(defn send-anxiety-email
+  [acct]
+  (prn
+    (str "Sending email to " (:email acct))))
+
+(defn mark-as-finished
+  [acct]
+  (korma/update mail_to_send
+    (korma/set-fields {:sent true})    
+    (korma/where {:id (:mail_id acct)})))
+
+(defn send-scheduled-emails
+  []
+  (map (fn [acct]
+         (do (mark-as-finished acct)
+           (send-anxiety-email acct)))
+    (fetch-emails-to-send)))
 
 
+(send-scheduled-emails)
